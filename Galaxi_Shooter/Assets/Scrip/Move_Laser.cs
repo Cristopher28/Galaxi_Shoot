fea572317a51a77b6426c4529bae8ae341ffe6b4
Cristopher28 <jorcristophersai@gmail.com>
2019-06-01 20:15:32 -0500

    Metodo para dispara
    Metodo para disparr y destruccion del proyectil a determinada distancia.

---
+++ Galaxi_Shooter/Assets/Scrip/Move_Laser.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move_Laser : MonoBehaviour
+{
+    //Este script se se encargara  de darle moviemiento al lazer 
+    //escribiremos una variable que tendra la velocidad  de el lazer 
+    // velocidad = speed
+    [SerializeField]
+    private float Speed = 5;
+
+
+    
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.up * Speed * Time.deltaTime);
+        
+        if(transform.position.y > 5.55f)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
--- Galaxi_Shooter/Assets/Scrip/Player_M.cs
+++ Galaxi_Shooter/Assets/Scrip/Player_M.cs
@@ -9,12 +9,14 @@ public class Player_M : MonoBehaviour
     //necesitaremos un control para el movimiento del personaje 
 
      public float Speed = 5;
+    [SerializeField]
+    private GameObject Laser_Prefab;
 
 
     
     void Start()
     {
-        transform.position = new Vector3(0, -3.65f, 0);
+        transform.position = new Vector3(0, 0, 0);
     }
 
     
@@ -26,29 +28,48 @@ public class Player_M : MonoBehaviour
         //aqui asemos el codigo dee traslado de nuestro player 
         transform.Translate(Vector3.right * horizontal * Speed * Time.deltaTime);
         transform.Translate(Vector3.up * vertical * Speed * Time.deltaTime);
-        //pondremos los limites en la pantalla para que nuestrom player no salga de pantalla 
-        //pseudo codigo
-        //si player es < -9.73 
-        //traladarse a + 9.73
-        //ocuparemos la condicion if 
-        if(transform.position.x < -9.73)
+        if (Input.GetKeyDown(KeyCode.Space))
         {
-            transform.position = new Vector3(9.73f,transform.position.y,0);
-        }else if(transform.position.x > 9.73)
-        {
-            transform.position = new Vector3(-9.73f, transform.position.y, 0);
+            Instantiate(Laser_Prefab,transform.position + new Vector3(0,0.83f,0) , Quaternion.identity);
         }
-
-        if(transform.position.y > 0)
-        {
-            transform.position = new Vector3(transform.position.x, 0, 0);
-        }else if(transform.position.y < -4.35)
-        {
-            transform.position = new Vector3(transform.position.x, -4.35f, 0);
-        }
+        Move_Player();
 
 
 
 
     }
+
+    void Move_Player()
+    {
+
+        //pondremos los limites en la pantalla para que nuestrom player no salga de pantalla 
+        //pseudo codigo
+        //si player.x es < -9.73 
+        //traladarse a + 9.73
+        //si player.y es < 0
+        // se quede en posision 0
+        // si player.y es menor a -4.73
+        //player se queda en la posicion -4.73
+        //ocuparemos la condicion if
+        if (transform.position.x < -9.73)
+        {
+            transform.position = new Vector3(9.73f, transform.position.y, 0);
+        }
+        else if (transform.position.x > 9.73)
+        {
+            transform.position = new Vector3(-9.73f, transform.position.y, 0);
+        }
+
+        if (transform.position.y > 0)
+        {
+            transform.position = new Vector3(transform.position.x, 0, 0);
+        }
+        else if (transform.position.y < -4.35)
+        {
+            transform.position = new Vector3(transform.position.x, -4.35f, 0);
+        }
+    }
+
+
+
 }
