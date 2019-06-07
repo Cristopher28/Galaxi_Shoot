4f76f000d39374a9c8d26e5c736ddba219c4d89a
Cristopher28 <jorcristophersai@gmail.com>
2019-06-07 07:19:58 -0500

    cambio de pocicion del enemigo al bajar
    al decender a 3.17 el enemigo se detiene

--- Galaxi_Shooter/Assets/enemy_move.cs
+++ Galaxi_Shooter/Assets/enemy_move.cs
@@ -8,13 +8,21 @@ public class enemy_move : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        transform.position = new Vector3(0, 6.5f, 0);
+        transform.position = new Vector3(Random.Range(-7f,7f), 6.5f, 0);
     }
 
     // Update is called once per frame
     void Update()
     {
+        //aqui detenemos al enemigo de bajar y se coloca en pocicion que se le indica en la pantalla 
+
         transform.Translate(Vector3.down * speed * Time.deltaTime);
+
+        if (transform.position.y <= 3.17)
+        {
+            transform.position = new Vector3(transform.position.x, 3.17f, 0);
+
+        }
 
         if (transform.position.y <= -6.5)
         {
